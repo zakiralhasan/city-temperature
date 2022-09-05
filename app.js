7030e7b65d7218afdd89ff6545751920539cc3f5
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-09-05 16:10:41 +0600

    get data from API

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 
+// gets the data from weather API 
+const getCityTemperature = (cityName) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=aea8d2188dcf9461a69803b36ba2f6de&units=metric`)
+    .then(res => res.json())
+    .then(data => processTempData(data))
+    .catch(error => consloe.log(error));
+};
+getCityTemperature('dhaka');
